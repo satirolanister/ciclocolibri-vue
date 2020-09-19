8d798785fe4bc09fd57ade37c63b5e1de8c34ad2
satirolanister <j.garzonsoto@hotmail.com>
2020-09-19 17:08:15 -0500

    login styles

--- componentes/login.js
+++ componentes/login.js
@@ -1,14 +1,14 @@
 Vue.component('login', {
     template: `
-    <div class=" container d-flex justify-content-center">
+    <div class="top">
         
         <form action="">
         <div class="form-group">
-        <label for="email">Email address:</label>
+        <label for="email" class="text-white">Email address:</label>
         <input type="email" class="form-control" id="email">
         </div>
         <div class="form-group">
-        <label for="pwd">Password:</label>
+        <label for="pwd" class="text-white">Password:</label>
         <input type="password" class="form-control" id="pwd">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
