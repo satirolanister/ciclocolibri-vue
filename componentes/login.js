4bc4035810164ab8f005efb57f11253fe06e1d5f
satirolanister <j.garzonsoto@hotmail.com>
2020-09-19 17:55:49 -0500

    login modificaion

--- componentes/login.js
+++ componentes/login.js
@@ -1,14 +1,15 @@
 Vue.component('login', {
     template: `
-    <div class="top">
-        
+    <div >
+        <img src="./img/logo.jpg" class="rounded-circle mx-auto d-block m-4 logo">
+        <br>
         <form action="">
         <div class="form-group">
-        <label for="email" class="text-white">Email address:</label>
+        <label for="email" >Email address:</label>
         <input type="email" class="form-control" id="email">
         </div>
         <div class="form-group">
-        <label for="pwd" class="text-white">Password:</label>
+        <label for="pwd" >Password:</label>
         <input type="password" class="form-control" id="pwd">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
